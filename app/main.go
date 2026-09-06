1398a38ad287a06e013664f65eee3630e0ed1bfa
nitnawareanshuman <anshumannitnaware7@gmail.com>
2026-09-07 00:54:04 +0530

    Implement exit

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,12 +12,18 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
+
 		fmt.Print("$ ")
 
 		command, err :=reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
+		}
+
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
 		}
 
 		fmt.Println(command[:len(command)-1] + ": command not found")
