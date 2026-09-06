10968eaffdd46a0cbb597aea473399c0f999123e
nitnawareanshuman <anshumannitnaware7@gmail.com>
2026-09-07 00:04:48 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,16 +6,20 @@ import (
 	"os"
 )
 
-
 func main() {
 
-	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+
+		command, err :=reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Printf(command[:len(command)-1] + ": command not found")
+	
 }
